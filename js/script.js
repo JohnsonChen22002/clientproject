7d5acf2f894736f4090e7a80b8790f5584370f87
Johnson Chen <johnsonchen12345@gmail.com>
2018-05-02 22:27:04 +0000

    weatherproofo3o

--- js/script.js
+++ js/script.js
@@ -1,8 +1,35 @@
 /* global $ */
-
-console.log('hi');
-
-$('h1').click(function(){
-    $('h1').css('color', 'green');
-    $('h1').text('Ready to Code');
+$(document).ready(function() {
+    var weatherUrl = "";
+    var apiKey = "&api_key=";
+    
+    function weatherURLWithSearchTerm(searchTerm) {
+        // write a function that will return a url for the giphy API with
+        // the searchTerm provided in the parameters
+        var newUrl = weatherUrl + searchTerm + apiKey;
+        return newUrl;
+    }
+    
+   
+    
+    function callWeatherAPIWithSearchTerm(searchTerm) {
+        // write a function that uses the above two functions to create the url,
+        // uses ajax to send a request, and appends the result to the body
+        $.ajax({
+            url: weatherURLWithSearchTerm(searchTerm),
+            method: "GET",
+            success: function(result) {
+                
+            },
+        });
+    }
+    
+     $("button").click(function() {
+        var input = $("#input").val();
+        console.log(input);
+    });
+    
+    
+    
+    
 });
