dabcce49f52db15ad7ea583231c2cf83315708d7
Johnson Chen <johnsonchen12345@gmail.com>
2018-05-16 22:10:43 +0000

    Chip

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* global $ */
 $(document).ready(function() {
-    var weatherUrl = "http://api.openweathermap.org/data/2.5/forecast?zip=";
+    var weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?zip=";
     var apiKey = ",us&APPID=970dbba6cad5524316250d5b1d1e77cc";
 
     function weatherURLWithSearchTerm(searchTerm) {
@@ -17,8 +17,10 @@ $(document).ready(function() {
                 console.log(result);
                 var temperature =  result.list[0].main.temp;
                 console.log(temperature);
-
-
+                var convert = Math.round(9/5*(temperature - 274) + 32)
+                console.log(convert)
+                $("#result").text("It is currently " + convert + "°F");
+                $("#logo").hide();
 
             },
         });
@@ -27,6 +29,7 @@ $(document).ready(function() {
     $("#formButton").click(function() {
         var input = $("#input").val();
         callWeatherAPIWithSearchTerm(input);
+        
     });
 
 
